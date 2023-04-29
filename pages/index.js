b4f2014fbb9e93b9753eaa00a6c8c33ce1866356
sachin <sachinsapkota773@gmail.com>
2023-04-29 17:24:05 +0545

    fixed navbar

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ const index = () => {
   return (
     <main className="flex items-center justify-center w-full h-full ">
       <div className=" shadow-2xl bg-gray-300  w-[80%]  mt-[100px] flex flex-col h-screen p-3">
-        <div className="flex flex-col w-full justify-between items-center bg-blue-700  h-[700px] mt-5 ">
+        <div className="flex  flex-col w-full justify-between items-center bg-blue-700  h-[700px] mt-5 ">
           <Image src={'/images/logo.png'} width={200} height={200} />
           hello
         </div>
